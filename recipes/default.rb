ce51cfb3ed2cd80af154947b98274ff501004c9a
Anshu Pande <anshu@adobe.com>
2014-08-13 14:33:10 -0400

    apache cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # 
 #
+package "httpd" do
+	action :install
+end
+
+service "httpd" do
+	#supports :status => true, :restart => true, :reload => true
+	action [ :start, :enable ]
+end
+
+template "/var/www/html/index.html" do
+	source "index.html.erb"
+	#owner "root"
+	#group "root"
+	#mode "0644"
+end
+
